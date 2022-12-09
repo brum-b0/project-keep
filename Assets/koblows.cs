70357bfbaf0378fa07a95b1ea737db6d4d2999c9
Nate Smith <smithn2@g.cofc.edu>
2022-12-09 15:47:31 -0500

    example

---
+++ Assets/koblows.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class koblows : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }//comment made by cobos
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

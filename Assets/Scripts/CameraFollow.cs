8cba96046e288f0123435112eebee7621488dfcf
Nate Smith <smithn2@g.cofc.edu>
2022-12-11 21:51:25 -0500

    cam fix

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,11 +6,13 @@ public class CameraFollow : MonoBehaviour
 {
     public float FollowSpeed = 2f;
     public Transform target;
+    public float yOffset = -0.2f;
+    public float xOffset = 0.2f;
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x, target.position.y, -10f);
-        transform.position = Vector3.Lerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
+        Vector3 newPos = new Vector3(target.position.x + xOffset, target.position.y + yOffset, -10f);
+        transform.position = Vector3.Lerp(transform.position, newPos, FollowSpeed );
     }
 }
